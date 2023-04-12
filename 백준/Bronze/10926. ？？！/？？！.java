21446cf59d3b076b9aad8f84556f8c3e8486215b
grimeeee <112916703+grimeeee@users.noreply.github.com>
2023-04-12 23:58:42 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,15 @@
+//joonas??!
+
+import java.util.Scanner;
+//import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String id = sc.next();
+        sc.close();
+        
+        System.out.print(id + "??!");
+        
+    }
+}
