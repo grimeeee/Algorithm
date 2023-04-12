700dc309a6631fd0231da176d2da7dd9f6e02f2e
grimeeee <112916703+grimeeee@users.noreply.github.com>
2023-04-12 23:05:56 +0900

    [Bronze V] Title: 고양이, Time: 120 ms, Memory: 13984 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,23 @@
+public class Main{
+    public static void main(String[] args){
+     
+    System.out.println("\\    /\\");  //역슬래시 주의
+    System.out.println(" )  ( ')");   
+    System.out.println("(  /  )");
+    System.out.println(" \\(__)|");
+    }        
+}
+
+
+
+
+
+/*
+예제 출력
+
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+
+*/
