56814be6b9f98e23b66a3e12482a68abbe6a92d9
grimeeee <112916703+grimeeee@users.noreply.github.com>
2023-04-12 22:59:54 +0900

    [Bronze V] Title: 개, Time: 120 ms, Memory: 13872 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,27 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+        
+/* 예제출력
+
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+        
+큰 따옴표 ( " ) 나 백슬래시 ( \ ) 는 단독으로 써서 출력할 수가 없다.
+그래서 이를 출력하기 위해 특정 구문을 사용하는데 이를 이스케이프 한다고 말한다.
+그리고 결합된 그 문자를 Escape Sequence  라고 하는데 백슬래시 ( \ ) 와 문자 하나를 결합하여 나타낸다.
+        
+        */
+    }
+}
+                         
+                         
+
+
