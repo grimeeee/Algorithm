a5415169d2e68df960d74c09b9810e4fb9df4140
grimeeee <112916703+grimeeee@users.noreply.github.com>
2023-04-12 23:21:02 +0900

    [Bronze V] Title: A-B, Time: 208 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,15 @@
+//두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+
+import java.util.Scanner;
+//import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        
+        System.out.println(a-b);
+    }
+}
