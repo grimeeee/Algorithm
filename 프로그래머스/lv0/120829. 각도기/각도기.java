d767d0b4295df24a03b4ce32b72c6473403863af
grimeeee <112916703+grimeeee@users.noreply.github.com>
2023-04-14 22:35:52 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.java
@@ -0,0 +1,21 @@
+
+
+class Solution {
+    public int solution(int angle) {
+        
+
+        
+      if ( angle > 0 && angle < 90 ){
+         return 1;
+      } else if ( angle == 90 ){
+         return 2;
+      } else if ( angle > 90 && angle < 180 ){
+         return 3;
+      } else if ( angle == 180 ) {
+         return 4;
+    }
+        
+        return 0;
+}
+   
+}
