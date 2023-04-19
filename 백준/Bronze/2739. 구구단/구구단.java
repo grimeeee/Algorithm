d73d62fca12c6b5bbf26b392b8e729c720b1f1b8
grimeeee <dpfla889@gmail.com>
2023-04-19 18:14:42 +0900

    [Bronze V] Title: 구구단, Time: 256 ms, Memory: 18668 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N;        
+        N = sc.nextInt();
+        sc.close();
+        
+        for(int i = 1; i<=9; i++)
+            System.out.println(N+" * "+i+" = "+(N*i));  
+    
+    }        
+}
