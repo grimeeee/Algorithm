3351f60b81926ddd2a3ec0e80e4553126c3848bd
grimeeee <dpfla889@gmail.com>
2023-04-24 19:34:54 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 116 ms, Memory: 13864 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,19 @@
+/*
+       _.-;;-._
+'-..-'|   ||   |
+'-..-'|_.-;;-._|
+'-..-'|   ||   |
+'-..-'|_.-''-._|
+
+*/
+
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-''-._|");
+    }
+}
