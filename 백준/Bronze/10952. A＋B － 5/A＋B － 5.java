ea349aa740ec2cf2d71c1ee7ee3cd0b7128a82d6
grimeeee <dpfla889@gmail.com>
2023-05-06 17:56:56 +0900

    [Bronze V] Title: A+B - 5, Time: 240 ms, Memory: 17900 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+    int A, B;
+	
+	while(true){
+	A = sc.nextInt();
+	B = sc.nextInt();
+	
+	if(A==0 && B==0){
+	sc.close();
+	break;
+	}
+	System.out.println(A + B);
+    }
+	
+}
+}
