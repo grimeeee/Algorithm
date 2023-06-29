30f9714f1858490b7c7a2e65a753f218e5287b0d
grimeeee <dpfla889@gmail.com>
2023-06-29 23:38:17 +0900

    Update 주사위 세개.java

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -1,30 +1,23 @@
 import java.util.Scanner;
 
-public class Main {
+public class Java_2480 {
     public static void main(String[] args) {
-        int a, b, c, max;
+        int a, b, c;
         Scanner sc = new Scanner(System.in);
         a = sc.nextInt();
         b = sc.nextInt();
         c = sc.nextInt();
 
-        if (a == b && b == c) {
-            System.out.println(10000 + a * 1000);
-        } else if (a == b && b != c) {
-            System.out.println(1000 + a * 100);
-        } else if (a != b && b == c) {
-            System.out.println(1000 + b * 100);
-        } else if (a != b && b != c && a == c) {
-            System.out.println(1000 + a * 100);
-        } else {
-            if ((a > b && b > c) || (a > b && b < c && a > c)) {
-                System.out.println(a * 100);
-            } else if ((a < b && b < c) || (a > b && b < c && a < c)) {
-                System.out.println(c * 100);
-            } else {
-                System.out.println(b * 100);
-            }
+        sc.close();
 
+        if (a == b && b == c) {
+            System.out.println(10000 + (a * 1000));
+        } else if (a == b || a == c) {
+            System.out.println(1000 + (a * 100));
+        } else if (b == c) {
+            System.out.println(1000 + (b * 100));
+        } else {
+            System.out.println((Math.max(a, Math.max(b, c))) * 100);
         }
     }
 
